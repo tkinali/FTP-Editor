6682565cf8f768bc9528274c4d8b04110f3d7c89
Tuncay KINALI <tkinali@gmail.com>
2016-09-25 22:38:20 +0300

    icons added

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 768})
+  mainWindow = new BrowserWindow({width: 1024, height: 768, icon: __dirname + '/assets/icons/icon.png'})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
